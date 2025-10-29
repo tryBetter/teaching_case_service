fd05eecbe1a1fad2766fb8a496bff0f330997857
liuwu <78924403@qq.com>
2025-10-30 00:13:45 +0800

    笔记移除字数限制

--- src/note/dto/create-note.dto.ts
+++ src/note/dto/create-note.dto.ts
@@ -9,10 +9,9 @@ import {
 
 export class CreateNoteDto {
   @ApiProperty({
-    description: '笔记内容',
+    description:
+      '笔记内容，支持字符串、JSON对象转换的字符串、Markdown字符串等大部分特殊格式的字符串数据',
     example: '这篇文章很有用，特别是关于NestJS的依赖注入部分。',
-    minLength: 1,
-    maxLength: 2000,
   })
   @IsString({ message: '笔记内容必须是字符串' })
   @IsNotEmpty({ message: '笔记内容不能为空' })
--- src/note/dto/update-note.dto.ts
+++ src/note/dto/update-note.dto.ts
@@ -4,11 +4,10 @@ import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateNoteDto extends PartialType(CreateNoteDto) {
   @ApiProperty({
-    description: '笔记内容',
+    description:
+      '笔记内容，支持字符串、JSON对象转换的字符串、Markdown字符串等大部分特殊格式的字符串数据',
     example:
       '更新后的笔记内容，这篇文章很有用，特别是关于NestJS的依赖注入部分。',
-    minLength: 1,
-    maxLength: 2000,
     required: false,
   })
   @IsOptional()
