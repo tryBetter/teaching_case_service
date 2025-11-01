4f9f64dc9df811adee1ecec6a00c8a4961f1f88f
liuwu <78924403@qq.com>
2025-11-01 23:19:06 +0800

    新增跨域白名单

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ async function bootstrap() {
         'http://127.0.0.1:8000',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'http://tauri.localhost',
       ];
 
   app.enableCors({
